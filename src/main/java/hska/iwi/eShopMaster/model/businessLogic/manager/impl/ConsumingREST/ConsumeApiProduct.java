7ca3d72210ed8c506b9a0ac94ef6f5dd51e2ddd6
and1kl <email.andreasklaiber@gmail.com>
2020-02-19 21:24:59 +0100

    erweiterung für oauth

--- src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ConsumingREST/ConsumeApiProduct.java
+++ src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/ConsumingREST/ConsumeApiProduct.java
@@ -1,10 +1,20 @@
 package hska.iwi.eShopMaster.model.businessLogic.manager.impl.ConsumingREST;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.impl.ProductManagerImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -16,25 +26,67 @@ public class ConsumeApiProduct {
     
     RestTemplate restTemplate = new RestTemplate();
 
+    public OAuth2ProtectedResourceDetails oAuth2ResourceDetails() {
+        //BaseOAuth2ProtectedResourceDetails details = new BaseOAuth2ProtectedResourceDetails();
+        //ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+        ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+
+        details.setClientId("apiProductId");
+        details.setClientSecret("apiProductSecret");
+        details.setAccessTokenUri("http://zuul:8092/oauth/token");
+        //details.setGrantType("client_credentials");
+        List<String> scope = new ArrayList<>();
+        scope.add("read");scope.add("write");
+        details.setScope(scope);
+        details.setAuthenticationScheme(AuthenticationScheme.header);
+        details.setClientAuthenticationScheme(AuthenticationScheme.header);
+        details.setId("1");
+        details.setTokenName("Api_Product");
+
+        //log.info("OAUTH DETAILS" + clientId + clientSecret + details.getAccessTokenUri() + details.getGrantType());
+        //details.set
+        return details;
+    }
+
+    @Bean // has to be done at runtime because the authorization server would not be up otherwise
+    public OAuth2RestTemplate foo() {
+        //OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(oAuth2ResourceDetails());
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+        return new OAuth2RestTemplate(oAuth2ResourceDetails(), new DefaultOAuth2ClientContext(atr));
+        /* To validate if required configurations are in place during startup */
+        //oAuth2RestTemplate.getAccessToken();
+        //return oAuth2RestTemplate;
+    }
+
     public Product[] getProducts() {
-        Product [] products = restTemplate.getForObject(urlApiProduct, Product[].class);
+        OAuth2RestTemplate restTemplate3 = foo();
+        Product [] products = restTemplate3.getForObject(urlApiProduct, Product[].class);
+        //Product [] products = restTemplate.getForObject(urlApiProduct, Product[].class);
         return products;
     }
 
     public void deleteProduct(int id) {
-        restTemplate.delete(urlApiProduct + "/" + id);
+        OAuth2RestTemplate restTemplate3 = foo();
+        restTemplate3.delete(urlApiProduct + "/" + id);
+        //restTemplate.delete(urlApiProduct + "/" + id);
     }
 
     public void updateProduct(Product product) {
-        restTemplate.put(urlApiProduct, product);
+        OAuth2RestTemplate restTemplate3 = foo();
+        restTemplate3.put(urlApiProduct, product);
+        //restTemplate.put(urlApiProduct, product);
     }
 
     public void deleteAllProducts() {
-        restTemplate.delete(urlApiProduct);
+        OAuth2RestTemplate restTemplate3 = foo();
+        restTemplate3.delete(urlApiProduct);
+        //restTemplate.delete(urlApiProduct);
     }
 
     public Product getProduct(int id) {
-        return restTemplate.getForObject(urlApiProduct + "/" + id, Product.class);
+        OAuth2RestTemplate restTemplate3 = foo();
+        return restTemplate3.getForObject(urlApiProduct + "/" + id, Product.class);
+        //return restTemplate.getForObject(urlApiProduct + "/" + id, Product.class);
     }
 
     public Product[] findProduct(Optional<String> searchValue, Optional<String> priceMinValue, Optional<String> priceMaxValue) {
@@ -119,12 +171,17 @@ public class ConsumeApiProduct {
 
         log.info("findProduct - Modified Request - final: "+request);
 
-        Product[] list = restTemplate.getForObject(request, Product[].class);
+
+        OAuth2RestTemplate restTemplate3 = foo();
+        Product[] list = restTemplate3.getForObject(request, Product[].class);
+        //Product[] list = restTemplate.getForObject(request, Product[].class);
         return list;
     }
 
     public void addProduct(Product product) {
-        restTemplate.postForLocation(urlApiProduct, product);
+        OAuth2RestTemplate restTemplate3 = foo();
+        restTemplate3.postForLocation(urlApiProduct, product);
+        //restTemplate.postForLocation(urlApiProduct, product);
     }
 
 }
